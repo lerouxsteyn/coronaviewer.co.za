af31963c6a863804f4437cdd2c6a3170a719a2b5
Lex <leroux@artificial.studio>
2020-07-10 12:10:51 +0200

    Update url

--- build/precache-manifest.2e794b24f05942c7417e68b00fc28ae9.js
+++ build/precache-manifest.2e794b24f05942c7417e68b00fc28ae9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "17f0ee06e3d27fcc45c0c12116970a5e",
+    "revision": "866f645a693ec2ea8fb721a6dc37bd06",
     "url": "/index.html"
   },
   {
-    "revision": "58010dd3d74d699dc2d3",
+    "revision": "a6a9e8466b3197f8327f",
     "url": "/static/css/main.6d3a7d4c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.7404ac4d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "58010dd3d74d699dc2d3",
-    "url": "/static/js/main.fbcabbd1.chunk.js"
+    "revision": "a6a9e8466b3197f8327f",
+    "url": "/static/js/main.60c2c943.chunk.js"
   },
   {
     "revision": "a8d3bc25cbeac7db4a84",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0417d1ca2a632cc94820da3199dbaa03.js"
+  "/precache-manifest.2e794b24f05942c7417e68b00fc28ae9.js"
 );
 
 self.addEventListener('message', (event) => {
