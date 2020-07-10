a1b920b7192df7e5c5efc2d5730e29a01e24c3f4
Lex <leroux@artificial.studio>
2020-07-10 09:25:52 +0200

    PHP script to convert CSV data into correct JSON format

---
+++ php/index.php
@@ -0,0 +1,81 @@
+<?php
+
+$data = [];
+$result = [];
+
+$url = [
+    'confirmed' => 'https://raw.githubusercontent.com/dsfsi/covid19za/master/data/covid19za_provincial_cumulative_timeline_confirmed.csv',
+    'deaths' => 'https://raw.githubusercontent.com/dsfsi/covid19za/master/data/covid19za_provincial_cumulative_timeline_deaths.csv',
+    'recovered' => 'https://raw.githubusercontent.com/dsfsi/covid19za/master/data/covid19za_provincial_cumulative_timeline_recoveries.csv',
+];
+
+$provinces = [
+    'EC' => 'Eastern Cape',
+    'FS' => 'Free State',
+    'GP' => 'Gauteng',
+    'KZN' => 'KwaZulu-Natal',
+    'LP' => 'Limpopo',
+    'MP' => 'Mpumalanga',
+    'NC' => 'Northern Cape',
+    'NW' => 'North West',
+    'WC' => 'Western Cape',
+];
+
+foreach(['confirmed', 'deaths', 'recovered'] as $type) {
+    $csv = file_get_contents($url[$type]);
+    $array = array_map("str_getcsv", explode("\n", $csv));
+    $data[$type] = $array;
+}
+
+function find_index_for_date($array, $date) {
+    foreach($array as $k => $v) {
+        if($v[1] == $date) {
+            return $k;
+        }
+    }
+}
+
+$i = 2;
+foreach($provinces as $code => $province) {
+    $result[$province] = [];
+    $last_confirmed = -1;
+    $last_deaths = -1;
+    $last_recovered = -1;
+
+    foreach(['confirmed'] as $type) {
+        foreach($data[$type] as $k => $row) {
+            if($k > 0 && $row[1] > 20200329) {
+                $confirmed = intval($row[$i]);
+                $deaths = intval($data['deaths'][find_index_for_date($data['deaths'], $row[1])][$i]);
+                $recovered = intval($data['recovered'][find_index_for_date($data['recovered'], $row[1])][$i]);
+
+                if($last_deaths > $deaths) $deaths = $last_deaths;
+                if($last_deaths > $deaths) $deaths = $last_deaths;
+                if($last_recovered > $recovered) $recovered = $last_recovered;
+
+                if($confirmed > $last_confirmed) $last_confirmed = $confirmed;
+                if($deaths > $last_deaths) $last_deaths = $deaths;
+                if($recovered > $last_recovered) $last_recovered = $recovered;
+
+                $result[$province][] = [
+                    'date' => $row[0],
+                    'confirmed' => $confirmed,
+                    'deaths' => $deaths,
+                    'recovered' => $recovered,
+                ];
+            }
+        }
+    }
+
+    $i++;
+}
+
+$counts = [
+    'confirmed' => count($data['confirmed']),
+    'deaths' => count($data['deaths']),
+    'recovered' => count($data['recovered']),
+];
+
+echo json_encode($result);
+
+?>
