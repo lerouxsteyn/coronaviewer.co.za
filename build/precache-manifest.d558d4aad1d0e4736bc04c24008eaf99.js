c003e79807492e5f2a65a6345535cf78f2264513
Le Roux <leroux@artificialstudio.com>
2020-07-28 00:00:40 +0000

    Data

--- build/precache-manifest.d558d4aad1d0e4736bc04c24008eaf99.js
+++ build/precache-manifest.d558d4aad1d0e4736bc04c24008eaf99.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7f589cbbc6e20fd3ca07302ab954793f",
+    "revision": "8c6eef1132156e0483441e3c4283b6f0",
     "url": "/index.html"
   },
   {
-    "revision": "9a95c3d5fa566195757c",
+    "revision": "a65c4f51ed4f750bd3f5",
     "url": "/static/css/main.6d3a7d4c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.498de678.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9a95c3d5fa566195757c",
-    "url": "/static/js/main.3b0960d4.chunk.js"
+    "revision": "a65c4f51ed4f750bd3f5",
+    "url": "/static/js/main.806094da.chunk.js"
   },
   {
     "revision": "a8d3bc25cbeac7db4a84",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4274989c34026de90946b3abda745c74.js"
+  "/precache-manifest.d558d4aad1d0e4736bc04c24008eaf99.js"
 );
 
 self.addEventListener('message', (event) => {
